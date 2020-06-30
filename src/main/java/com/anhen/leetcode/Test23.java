d31beae08d5efe296d112f80e13ad172c842e89f
1004682390@qq.com <wxj121987>
2020-06-30 23:05:04 +0800

    fea：LeetCode，23 合并K个排序链表 分治，最小堆

--- src/main/java/com/anhen/leetcode/Test23.java
+++ src/main/java/com/anhen/leetcode/Test23.java
@@ -1,5 +1,8 @@
 package com.anhen.leetcode;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * @ClassName Test23
  * @Description
@@ -46,7 +49,27 @@ public class Test23 {
         return res;
     }
 
-    //方法一：逐一合并两条链表
+    //方法二：分治，两两合并
+    private ListNode mergeKLists2(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    //通过mid将数组一分为二，并不断缩小规模，当规模为1时返回并开始合并
+    //通过合并两个链表，不断增大其规模，整体看就是不断缩小-最后不断扩大的过程
+    private ListNode merge(ListNode[] lists, int lo, int hi) {
+        if (lo == hi) {
+            return lists[lo];
+        }
+        int mid = lo + (hi - lo) / 2;
+        ListNode l1 = merge(lists, lo, mid);
+        ListNode l2 = merge(lists, mid + 1, hi);
+        return mergeTwoLists(l1, l2);
+    }
+
+    //迭代合并两条链表
     private  ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode result = new ListNode(0);
         ListNode head = result;
@@ -62,6 +85,36 @@ public class Test23 {
         }
         result.next = l1 == null ? l2 : l1;
 
+        return head.next;
+    }
+
+    //
+    //方法三：最小堆
+    private ListNode mergeKLists3(ListNode[] lists) {
+        if(lists==null || lists.length==0) {
+            return null;
+        }
+        //创建一个堆，并设置元素的排序方式
+        PriorityQueue<ListNode> queue = new PriorityQueue(new Comparator<ListNode>() {
+            public int compare(ListNode o1, ListNode o2) {
+                return (o1.val - o2.val);
+            }
+        });
+        //遍历链表数组，然后将每个链表的每个节点都放入堆中
+        for(int i=0;i<lists.length;i++) {
+            while(lists[i] != null) {
+                queue.add(lists[i]);
+                lists[i] = lists[i].next;
+            }
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        //从堆中不断取出元素，并将取出的元素串联起来
+        while( !queue.isEmpty() ) {
+            dummy.next = queue.poll();
+            dummy = dummy.next;
+        }
+        dummy.next = null;
         return head.next;
     }
 }
