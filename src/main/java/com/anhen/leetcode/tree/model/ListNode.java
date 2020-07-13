c82b56bbea029f38544d12335d66e9919a70ecfd
wuxiaojian <1004682390@qq.com>
2020-07-13 20:45:31 +0800

    feat: leetcode 1367 树 广度优先遍历+深度优先遍历

---
+++ src/main/java/com/anhen/leetcode/tree/model/ListNode.java
@@ -0,0 +1,18 @@
+package com.anhen.leetcode.tree.model;
+
+import com.anhen.leetcode.tree.Test1367;
+
+/**
+ * @classname: ListNode
+ * @description: ListNode
+ * @author: wuxiaojian
+ * @create: 2020-07-13 20:05
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/anhen/leetcode/tree/model/TreeNode.java
@@ -0,0 +1,18 @@
+package com.anhen.leetcode.tree.model;
+
+/**
+ * @classname: TreeNode
+ * @description: TreeNode
+ * @author: wuxiaojian
+ * @create: 2020-07-13 20:03
+ */
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
