32093c73138c33d75c483497fb934d28404bcb39
wuxiaojian <1004682390@qq.com>
2020-08-19 20:00:29 +0800

    feat: rpc demo

--- src/main/java/com/goldcard/rpc/example/RpcFramework.java
+++ src/main/java/com/goldcard/rpc/example/RpcFramework.java
@@ -22,10 +22,12 @@ public class RpcFramework {
      * @throws Exception 
      */  
     public static void export(final Object service, int port) throws Exception {  
-        if (service == null)  
-            throw new IllegalArgumentException("service instance == null");  
-        if (port <= 0 || port > 65535)  
-            throw new IllegalArgumentException("Invalid port " + port);  
+        if (service == null) {
+            throw new IllegalArgumentException("service instance == null");
+        }
+        if (port <= 0 || port > 65535) {
+            throw new IllegalArgumentException("Invalid port " + port);
+        }
         System.out.println("Export service " + service.getClass().getName() + " on port " + port);  
         ServerSocket server = new ServerSocket(port);  
         for(;;) {  
@@ -80,41 +82,36 @@ public class RpcFramework {
      */  
     @SuppressWarnings("unchecked")  
     public static <T> T refer(final Class<T> interfaceClass, final String host, final int port) throws Exception {  
-        if (interfaceClass == null)  
-            throw new IllegalArgumentException("Interface class == null");  
-        if (! interfaceClass.isInterface())  
-            throw new IllegalArgumentException("The " + interfaceClass.getName() + " must be interface class!");  
-        if (host == null || host.length() == 0)  
-            throw new IllegalArgumentException("Host == null!");  
-        if (port <= 0 || port > 65535)  
-            throw new IllegalArgumentException("Invalid port " + port);  
+        if (interfaceClass == null) {
+            throw new IllegalArgumentException("Interface class == null");
+        }
+        if (! interfaceClass.isInterface()) {
+            throw new IllegalArgumentException("The " + interfaceClass.getName() + " must be interface class!");
+        }
+        if (host == null || host.length() == 0) {
+            throw new IllegalArgumentException("Host == null!");
+        }
+        if (port <= 0 || port > 65535) {
+            throw new IllegalArgumentException("Invalid port " + port);
+        }
         System.out.println("Get remote service " + interfaceClass.getName() + " from server " + host + ":" + port);  
         return (T) Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class<?>[] {interfaceClass}, new InvocationHandler() {  
             @Override
             public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
-                Socket socket = new Socket(host, port);  
-                try {  
-                    ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());  
-                    try {  
-                        output.writeUTF(method.getName());  
-                        output.writeObject(method.getParameterTypes());  
-                        output.writeObject(arguments);  
-                        ObjectInputStream input = new ObjectInputStream(socket.getInputStream());  
-                        try {  
-                            Object result = input.readObject();  
-                            if (result instanceof Throwable) {  
-                                throw (Throwable) result;  
-                            }  
-                            return result;  
-                        } finally {  
-                            input.close();  
-                        }  
-                    } finally {  
-                        output.close();  
-                    }  
-                } finally {  
-                    socket.close();  
-                }  
+                try (Socket socket = new Socket(host, port)) {
+                    try (ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream())) {
+                        output.writeUTF(method.getName());
+                        output.writeObject(method.getParameterTypes());
+                        output.writeObject(arguments);
+                        try (ObjectInputStream input = new ObjectInputStream(socket.getInputStream())) {
+                            Object result = input.readObject();
+                            if (result instanceof Throwable) {
+                                throw (Throwable) result;
+                            }
+                            return result;
+                        }
+                    }
+                }
             }  
         });  
     }  
