04011c4a126c841f3074331f9f4c6edbdf4b1aeb
wuxiaojian <1004682390@qq.com>
2020-09-27 14:02:09 +0800

    feat: activiti demo

---
+++ src/main/java/com/anhen/activiti/Test.java
@@ -0,0 +1,13 @@
+package com.anhen.activiti;
+
+import org.activiti.engine.impl.db.DbSchemaCreate;
+
+/**
+ * @author wuxiaojian
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        DbSchemaCreate.main(args);
+    }
+}
