0dbcfebc1742409297203e18412fd98419082958
wuxiaojian <1004682390@qq.com>
2021-04-09 18:28:42 +0800

    feat: lambda flatmap

---
+++ src/main/java/com/jdk8/function/TestFlatMap.java
@@ -0,0 +1,48 @@
+package com.jdk8.function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @classname: TestFlatMap
+ * @description: TODO
+ * @author: wuxiaojian
+ * @create: 2021-04-09 14:12
+ */
+public class TestFlatMap {
+    public static void main(String[] args) {
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(3, 4);
+        List<int[]> pairs =
+                numbers1.stream()
+                        .flatMap(i ->
+                                numbers2.stream()
+                                        .filter(j -> (i + j) % 3 == 0)
+                                        .map(j -> new int[]{i, j})
+                        )
+                        .collect(toList());
+
+        //100内符合勾股定理的数组
+        Stream<double[]> pythagoreanTriples =
+                IntStream.rangeClosed(1, 100).boxed()
+                        .flatMap(a ->
+                                IntStream.rangeClosed(a, 100)
+                                        .mapToObj(
+                                                b -> new double[]{a, b, Math.sqrt(a*a + b*b)})
+                                        .filter(t -> t[2] % 1 == 0));
+
+        pythagoreanTriples.limit(5)
+                .forEach(t ->
+                        System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+
+        //斐波那契数列
+        Stream.iterate(new int[]{0, 1},
+                t -> new int[]{t[1], t[0]+t[1]})
+                .limit(20)
+                .forEach(t -> System.out.println("(" + t[0] + "," + t[1] +")"));
+    }
+}
