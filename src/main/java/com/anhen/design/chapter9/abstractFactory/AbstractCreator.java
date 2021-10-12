24bb2aa0b8a3c7f327b5021bcc4df43fe60545bb
wuxiaojian <1004682390@qq.com>
2021-10-12 10:06:51 +0800

    feat: 抽象工厂模式

---
+++ src/main/java/com/anhen/design/chapter9/abstractFactory/AbstractCreator.java
@@ -0,0 +1,9 @@
+package com.anhen.design.chapter9.abstractFactory;
+
+public abstract class AbstractCreator {
+    //创建A产品家族
+    public abstract AbstractProductA createProductA();
+
+    //创建B产品家族
+    public abstract AbstractProductB createProductB();
+}
---
+++ src/main/java/com/anhen/design/chapter9/abstractFactory/ProductB1.java
@@ -0,0 +1,8 @@
+package com.anhen.design.chapter9.abstractFactory;
+
+public class ProductB1 extends AbstractProductB {
+    @Override
+    public void doSomething() {
+        System.out.println("产品B1的实现方法");
+    }
+}
