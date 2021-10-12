5759d91f6b4420e01bbbe6a8c17f24fb084f433c
wuxiaojian <1004682390@qq.com>
2021-10-12 17:06:05 +0800

    feat: 建造者模式

---
+++ src/main/java/com/anhen/design/chapter11/builder/BenzBuilder.java
@@ -0,0 +1,17 @@
+package com.anhen.design.chapter11.builder;
+
+import java.util.ArrayList;
+
+public class BenzBuilder extends CarBuilder {
+    private BenzModel benz = new BenzModel();
+
+    @Override
+    public CarModel getCarModel() {
+        return this.benz;
+    }
+
+    @Override
+    public void setSequence(ArrayList<String> sequence) {
+        this.benz.setSequence(sequence);
+    }
+}
