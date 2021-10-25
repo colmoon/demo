e447b04a1348a1899bdb2780e96c023dd68bce21
wuxiaojian <1004682390@qq.com>
2021-10-25 19:11:45 +0800

    feat: 命令者模式

---
+++ src/main/java/com/anhen/design/chapter15/command/ConcreteReciver2.java
@@ -0,0 +1,8 @@
+package com.anhen.design.chapter15.command;
+
+public class ConcreteReciver2 extends Receiver {
+    //每个接收者都必须处理一定的业务逻辑
+    @Override
+    public void doSomething() {
+    }
+}
---
+++ src/main/java/com/anhen/design/chapter15/command/instance/AddRequirementCommand.java
@@ -0,0 +1,13 @@
+package com.anhen.design.chapter15.command.instance;
+
+public class AddRequirementCommand extends Command {
+    //执行增加一项需求的命令
+    @Override
+    public void execute() {
+        //找到需求组
+        super.rg.find();
+        //增加一份需求super.rg.add();
+        //给出计划
+        super.rg.plan();
+    }
+}
