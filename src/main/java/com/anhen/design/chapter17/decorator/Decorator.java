aa235ba29f8592fbf332d2c443a16ae88476469a
wuxiaojian <1004682390@qq.com>
2021-11-01 18:30:23 +0800

    feat: 装饰者模式

---
+++ src/main/java/com/anhen/design/chapter17/decorator/Decorator.java
@@ -0,0 +1,20 @@
+package com.anhen.design.chapter17.decorator;
+
+/**
+ * @classname: Decorator
+ * @description: 抽象装饰者
+ * @author: wuxiaojian
+ * @create: 2021-10-27 20:23
+ */
+public abstract class Decorator extends Component{
+    private Component component;
+
+    public Decorator(Component component){
+        this.component = component;
+    }
+
+    @Override
+    public void operate() {
+        this.component.operate();
+    }
+}
