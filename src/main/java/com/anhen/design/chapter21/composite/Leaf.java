4fdd9f83ca17cb15c92af05d15712beaf89bfac3
wuxiaojian <1004682390@qq.com>
2021-11-06 17:24:11 +0800

    feat: 组合模式

---
+++ src/main/java/com/anhen/design/chapter21/composite/Leaf.java
@@ -0,0 +1,15 @@
+package com.anhen.design.chapter21.composite;
+
+/**
+ * @classname: Leaf
+ * @description: TODO
+ * @author: wuxiaojian
+ * @create: 2021-11-06 16:35
+ */
+public class Leaf extends Component {
+
+    @Override
+    public void doSomething(){
+
+    }
+}
